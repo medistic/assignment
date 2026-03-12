a1a58996d9dfa2e41dc07f39cc143dc096482c06
medistic <106013179+medistic@users.noreply.github.com>
2026-03-12 10:33:27 +0900

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 
 int main() {
 
 
     int neg = 0;
-    float a;
-    scanf("%f",a);
+    float a=0;
+    scanf("%f", &a);
     int inte = a;
     float real = a - inte;
 
@@ -64,6 +65,8 @@ int main() {
             if (arr1[j] == 2) {
                 if (j == 0) {
                     list[7] += 1;
+                    arr1[j] = 0;
+                    break;
                 }
                 arr1[j] = 0;
                 arr1[j - 1] += 1;
@@ -72,7 +75,7 @@ int main() {
     }
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if (arr1[j] == 2) {
+            if (list[j] == 2) {
 
                 list[j] = 0;
                 if (j == 0) {

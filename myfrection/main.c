23fd3154c88b7525fec2781677937862fddd5afa
medistic <106013179+medistic@users.noreply.github.com>
2026-03-20 00:31:16 +0900

    Update main.c

--- myfrection/main.c
+++ myfrection/main.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "my_fraction.h"
 
@@ -15,67 +16,120 @@ int main() {
 	fractions num2;
 	num2.den = 0;
 	num2.numer = 0;
+	char type[1] = { 'a' };
+	int end = 0;
+	char str1[1000];
+	char str2[1000];
 	char clist[1] = { 'a', };
 	char opra = 'a';
-	int i = 0;
-	int j = 0;
+	char* dvd;
+	float x = 0.0;
 	int xnumer = 0;
 	int xden = 1;
-	float x = 0.0;
-	float y = 0.0;
-	int file = -1;
-	while (file >= 3 || file < 0) {
-		printf("Enter the number of the file you want to read from (fraction:0, integer:1, float:2\n type: ");
-		scanf(" %d", &file);
-		if (file >= 3 || file < 0) {
-			printf("put number between 0,3");
-			continue;
+
+	scanf("%s", str1);
+	scanf(" %c", &opra);
+	scanf("%s", str2);
+	
+	for (int i = 0; i < sizeof(str1);i++) {
+		if (str1[i] == '/') {
+			type[0] = 'f';
+			break;
+		}
+		if (str1[i] == '.') {
+			type[0] = 'F';
+			break;
+		}
+		else
+		{
+			type[0] = 'i';
 		}
 	}
-	if (file == 0) {
-		scanf(" %d%c%d", &num1.numer, &clist[0], &num1.den);
-		scanf(" %c", &opra);
-		scanf(" %d%c%d", &num2.numer, &clist[0], &num2.den);
-		printf(" %d/%d\n", num1.numer, num1.den);
-		printf(" %d/%d\n", num2.numer, num2.den);
+	if(type[0]=='f'){
+		dvd = strtok(str1, "/");
+		if (dvd != NULL) {
+			num1.numer = atoi(dvd);
+			dvd = strtok(NULL, "/");
+			num1.den = atoi(dvd);
+		}
 	}
-	else if (file == 1) {
-		scanf(" %d", &i);
-		scanf(" %c", &opra);
-		scanf(" %d", &j);
+	
+	if (type[0] == 'i') {
 		num1.den = 1;
-		num1.numer = i;
-		num2.den = 1;
-		num2.numer = j;
-		printf(" %d/%d\n", num1.numer, num1.den);
-		printf(" %d/%d\n", num2.numer, num2.den);
+		num1.numer = atoi(str1);
 	}
-	else if (file == 2) {
-		scanf(" %f", &x);
-		scanf(" %c", &opra);
-		scanf(" %f", &y);
+
+	if (type[0] == 'F') {
+		
+		x = atof(str1);
+		
 		for (int i = 0;i < 23;i++){
 			xnumer = x;
 			if (x - xnumer != 0.0) {
 				x = x * 10;
 				xden = xden * 10;
+				
+				printf("[%d]\n", xnumer);
+				printf("[%d]\n", xden);
+				continue;
 			}
 			else {
 				num1.den = xden;
 				num1.numer = xnumer;
+				printf("[%d]\n", xnumer);
+				printf("[%d]\n", xden);
 			}
 		}
+
+	}
+	for (int i = 0; i < sizeof(str2);i++) {
+		if (str2[i] == '/') {
+			type[0] = 'f';
+			break;
+		}
+		if (str2[i] == '.') {
+			type[0] = 'F';
+			break;
+		}
+		else
+		{
+			type[0] = 'i';
+		}
+	}
+	if (type[0] == 'f') {
+		dvd = strtok(str2, "/");
+		if (dvd != NULL) {
+			num2.numer = atoi(dvd);
+			dvd = strtok(NULL, "/");
+			num2.den = atoi(dvd);
+		}
+	}
+
+	if (type[0] == 'i') {
+		num2.den = 1;
+		num2.numer = atoi(str2);
+	}
+
+	if (type[0] == 'F') {
+
+		x = atof(str2);
+
 		for (int i = 0;i < 23;i++) {
-			xnumer = y;
-			if (y - xnumer != 0.0) {
-				y = y * 10;
+			xnumer = x;
+			if (x - xnumer != 0.0) {
+				x = x * 10;
 				xden = xden * 10;
+
+				
+				continue;
 			}
 			else {
 				num2.den = xden;
 				num2.numer = xnumer;
+				
 			}
 		}
+
 	}
 	
 	if (num1.den == 0)

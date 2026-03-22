cb27dd75ce6e1279a025f77ff4fecf86ad59fd1b
medistic <106013179+medistic@users.noreply.github.com>
2026-03-22 19:31:58 +0900

    Create my_fraction.h

---
+++ square/my_fraction.h
@@ -0,0 +1,16 @@
+typedef struct value{
+	int den;
+	int numer;
+}fractions;
+
+//통분하기
+extern int denom( fractions i,fractions j);
+//사칙연산
+extern fractions add(fractions i, fractions j);
+extern fractions minus(fractions i, fractions j);
+extern fractions multi(fractions i, fractions j);
+extern fractions divis(fractions i, fractions j);
+//약분하기
+extern fractions abbre(fractions i);
+//연산범위 검사
+extern int testb(fractions i, fractions j, char a);

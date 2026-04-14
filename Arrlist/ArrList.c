378103f813371654a8108fc8221eb4f0d13d468b
medistic <106013179+medistic@users.noreply.github.com>
2026-04-14 10:11:53 +0900

    Update ArrList.c

--- Arrlist/ArrList.c
+++ Arrlist/ArrList.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "9_1.h"
+typedef int elementArrayList;
+typedef struct arrayListType {
+	elementArrayList * data;
+	elementArrayList size;
+	elementArrayList capacity;
 
-int main() {
-	
-}
+}arrayList;
+
+
+//struct arrayListType {
+//	elementArrayList* data;
+//	elementArrayList size;
+//	elementArrayList capacity;
+//
+//};
+//typedef struct arrayListType arrayList;
+//와 같다
+
+extern arrayList* creatArrayList(int size);
+extern void distroyArrayList(arrayList* al);
+
+extern int isEmptyArrayList(arrayList* al);
+extern int isFullArrayList(arrayList* al);
+extern int sizeArrayList(arrayList* al);
+
+extern int insertArrayList(arrayList* al, int pos, elementArrayList item);
+extern elementArrayList deleteArrayList(arrayList* al, int pos);
+extern void initArrayList(arrayList* al);
+
+extern elementArrayList getitemArrayList(arrayList* al, int pos);
+extern int replaaceItemArrayList(arrayList* al, int pos, elementArrayList item);
+
+extern elementArrayList printArrayList(arrayList* al);
